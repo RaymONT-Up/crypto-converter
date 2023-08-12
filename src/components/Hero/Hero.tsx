b077892640dfc6cba14458de8f0664f58d2f8149
Raym0NT <konasr6@gmail.com>
2023-08-13 00:24:55 +0300

    hero block animation bg

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,11 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import cls from "./Hero.module.scss";
 import classNames from "../../utils/classNames/classNames";
 import Title, { TitleTags, TitleTheme } from "../UI/Title/Title";
 import AppLink, { AppLinkTheme } from "../UI/AppLink/AppLink";
+
+import * as THREE from "three";
+import HALO from "vanta/dist/vanta.halo.min";
 
 interface HeroProps {
   className?: string;
@@ -10,8 +13,31 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = (props) => {
   const { className } = props;
+
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        HALO({
+          el: myRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          baseColor: 0x0,
+          backgroundColor: 0x0,
+          size: 1.2,
+          speed: 0,
+        })
+      );
+    }
+  }, [vantaEffect]);
+
   return (
-    <div className={classNames(cls.Hero, {}, [className || ""])}>
+    <div ref={myRef} className={classNames(cls.Hero, {}, [className || ""])}>
       <div className={cls.content}>
         <Title
           className={cls.title}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+declare module 'vanta/dist/vanta.globe.min';
+declare module 'vanta/dist/vanta.halo.min'
+declare module 'three'
